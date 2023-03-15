bacbbbef546f21a32ff41fcfea11dc8ce72407a2
Andreas Schlicker (gfkbf) <andreas.schlicker@bayer.com>
2023-03-15 20:49:39 +0100

    adapt rat STAR reference

--- META/STAR.param.rat.sh
+++ META/STAR.param.rat.sh
@@ -1,2 +1,2 @@
-local REFERENCE=$dir_to_reference$species/"STAR/"
+local REFERENCE=$dir_to_reference$species/"Rattus_norvegicus.mRatBN7.2.dna_rm.primary_assembly/"
 local GTF=$dir_to_reference$species/"Rattus_norvegicus.mRatBN7.2.108.chr.gtf"
